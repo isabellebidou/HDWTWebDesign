297591ca19b1581ba06a4873f1a8208859bf5073
Isabelle <isa.bidou@gmail.com>
2018-04-25 16:32:43 +0100

    isabelle adding email and tel in contact page

--- behaviour/behaviour.js
+++ behaviour/behaviour.js
@@ -4,7 +4,6 @@ var map = null;
 
 var element = null;
 
-
 function initialize() {
 	console.log("initialize!");
 	mobileMenuDiv = document.getElementById("mobilemenu");
@@ -37,35 +36,35 @@ function resize() {
 	location.reload(true);
 }
 
-function contactInitialize(){
+function contactInitialize() {
 	element = document.getElementById("map");
 	map = new google.maps.Map(element, {
-	center : new google.maps.LatLng(53.3489867, -6.2434582), // we insert the default values of the location -->
-	zoom : 16, // level is entered to show the position appropriately-->
-	mapTypeId : "OSM",
-	mapTypeControl : true,
-	streetViewControl : true,
-	src :"http://maps.googleapis.com/maps/api/js?sensor=false"
-});
-map.mapTypes.set("OSM", new google.maps.ImageMapType({
-	getTileUrl : function(coord, zoom) {
-		var tilesPerGlobe = 1 << zoom;
-		var x = coord.x % tilesPerGlobe;
-		if (x < 0) {
-			x = tilesPerGlobe + x;
-		}
-		return "http://tile.openstreetmap.org/" + zoom + "/" + x + "/" + coord.y + ".png";
-	},
-	marker : new google.maps.Marker({
-		position : {
-			lat : 53.3489867,
-			lng : -6.2434582
+		center : new google.maps.LatLng(53.3489867, -6.2434582), // we insert the default values of the location -->
+		zoom : 16, // level is entered to show the position appropriately-->
+		mapTypeId : "OSM",
+		mapTypeControl : true,
+		streetViewControl : true,
+		src : "http://maps.googleapis.com/maps/api/js?sensor=false"
+	});
+	map.mapTypes.set("OSM", new google.maps.ImageMapType({
+		getTileUrl : function(coord, zoom) {
+			var tilesPerGlobe = 1 << zoom;
+			var x = coord.x % tilesPerGlobe;
+			if (x < 0) {
+				x = tilesPerGlobe + x;
+			}
+			return "http://tile.openstreetmap.org/" + zoom + "/" + x + "/" + coord.y + ".png";
 		},
-		map : map,
-		title : 'Hello World!'
-	}),
-	tileSize : new google.maps.Size(256, 256),
-	name : "OpenStreetMap",
-	maxZoom : 18
-}));
+		marker : new google.maps.Marker({
+			position : {
+				lat : 53.3489867,
+				lng : -6.2434582
+			},
+			map : map,
+			title : 'Hello World!'
+		}),
+		tileSize : new google.maps.Size(256, 256),
+		name : "OpenStreetMap",
+		maxZoom : 18
+	}));
 }
